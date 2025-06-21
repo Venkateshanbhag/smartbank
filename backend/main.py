b4bbcc0e30bcd562e417c45617bda4d5b62efd9c
Venkateshanbhag <137861124+Venkateshanbhag@users.noreply.github.com>
2025-06-21 22:55:11 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,10 @@ import smtplib
 from email.message import EmailMessage
 
 # Initialize app and CORS
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+
+
 app = Flask(__name__)
 CORS(app)
 
